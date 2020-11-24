e35fa9903d250e1dd56c1e1ee4bfea9291e8d93d
adnapp <adam.napps@gmail.com>
2020-11-24 16:31:44 -0500

    next draft

---
+++ app/controllers/missions_controller.rb
@@ -0,0 +1,16 @@
+class MissionsController < ApplicationController
+    def new 
+        @mission = Mission.new
+    end
+
+    def create 
+        @mission = Mission.create(mission_params)
+        redirect_to scientist_path(@mission.scientist)
+    end
+
+
+    private
+    def mission_params
+        params.require(:mission).permit(:name, :scientist_id, :planet_id)
+    end
+end
